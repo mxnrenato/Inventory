1d58a8adb2fbf7300da09b6f2fa4f3d25fd5e5b9
mxnrenato <maximilianoriveraabad@gmail.com>
2021-12-28 15:11:04 -0500

    Add project files.

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        public string CategoryId { get; set; }
+        public int MyProperty { get; set; }
+
+    }
+}
